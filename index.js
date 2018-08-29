c0e5156afd65f223c872ab025489f8f3c444a27b
Omid Vatandoost <omidvatan94@gmail.com>
2018-08-29 19:15:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length)
+  return kittens.slice(0, kittens.length - 1)
 }
